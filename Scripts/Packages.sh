e158ef4faa9f4a1874983bae2fec2b0c92bb7695
jack20250207111-ops <jack20250207111@gmail.com>
2026-04-16 16:18:14 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -58,6 +58,7 @@ UPDATE_PACKAGE() {
 #UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "master" "pkg"
 #UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 #UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
+UPDATE_PACKAGE "xray-core" "XTLS/Xray-core" "main"
 
 #UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
 
