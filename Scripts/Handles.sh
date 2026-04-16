dbb84555717d646c898bb7573ec686e4ece74d39
jack20250207111-ops <jack20250207111@gmail.com>
2026-04-16 16:25:11 +0800

    Refactor Handles.sh for surge rules cloning and cleanup
    Updated the script to clone surge rules directly into the 'surge' directory and removed unnecessary echo statements. Adjusted the directory navigation and cleanup steps.

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -4,15 +4,13 @@ PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
 #预置HomeProxy数据
 if [ -d *"homeproxy"* ]; then
-	echo " "
-
-	HP_RULE="surge"
+	PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 	HP_PATH="homeproxy/root/etc/homeproxy"
 
 	rm -rf ./$HP_PATH/resources/*
 
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
-	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
+	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./surge
+	cd ./surge && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
 
 	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
 	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
@@ -28,7 +26,7 @@ fi
 if [ -d *"luci-theme-argon"* ]; then
 	echo " "
 
-	cd ./luci-theme-argon/
+	cd .. && rm -rf ./surge
 
 	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
 
